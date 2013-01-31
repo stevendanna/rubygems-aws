439320faeeb85ca2b2c26dabfd6e3cece7fc57a2
Sean OMeara <someara@opscode.com>
2013-01-31 14:44:31 -0500

    removing application attributes from rubygems role. josh will refactor into databags

--- chef/roles/rubygems.rb
+++ chef/roles/rubygems.rb
@@ -12,38 +12,3 @@ run_list(
   "recipe[rubygems::rails]",
   "recipe[rubygems::rails_nginx]"
 )
-
-default_attributes(
-  "application" => {
-    "name" => "rubygems",
-    "repository" => "https://github.com/rubygems/rubygems.org.git",
-    "rails_env" => "staging",
-    "rails_root" => "/applications/rubygems/staging",
-    "server_names" => ["rubygems.phlippers.net"],
-    "use_ssl" => true,
-    "force_ssl" => true,
-    "ssl_key" => "dev.rubygems.org.key",
-    "ssl_cert" => "dev.rubygems.org.crt",
-    "app_server" => {
-      "name" => "thin",
-      "concurrency" => 4
-    }
-  },
-  "environment_variables" => {
-    "RAILS_ENV" =>  "staging",
-    "RACK_ENV" =>   "staging",
-    "REDIS_URL" =>  "redis://localhost:6379/0",
-
-    "TMOUT" => "600"
-
-    # // Ruby GC tuning
-    # // "RUBY_HEAP_MIN_SLOTS" => 1000000,
-    # // "RUBY_HEAP_SLOTS_INCREMENT" => 1000000,
-    # // "RUBY_HEAP_SLOTS_GROWTH_FACTOR" => 1,
-    # // "RUBY_GC_MALLOC_LIMIT" => 1000000000,
-    # // "RUBY_HEAP_FREE_MIN" => 500000,
-    # // "RUBY_FREE_MIN" => 500000
-  },
-
-  "set_fqdn" => "rubygems.phlippers.net"
-)
