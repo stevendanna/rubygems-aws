aeb2fd6acb7b3c0614fc07b71f54503de2480c53
Steven Danna <steve@opscode.com>
2013-02-01 21:36:55 -0800

    Use custom built nginx for load balancer.

--- chef/roles/balancer.rb
+++ chef/roles/balancer.rb
@@ -2,7 +2,14 @@ name "balancer"
 description "The role which contains all cookbooks for a load balancer."
 run_list(
   "role[base]",
-  "recipe[nginx::server]",
-  "recipe[rubygems::balancer]",
-  "recipe[rubygems::rails]"
+  "recipe[rubygems::nginx_source]",
+  "recipe[rubygems::balancer]"
+)
+
+override_attributes(
+  "nginx" => {
+    "geoip" => true,
+    "enable_stub_status" => false,
+    "dir" => "/opt/nginx/conf"
+  }
 )
